094d010454ebf393b5ecfc0e9e572880440f833f
UpwindPlains70 <69139852+UpwindPlains70@users.noreply.github.com>
2021-08-30 12:06:00 +0800

    GameManager
    stores info for all levels throughout game (primitive state)

---
+++ Virtual Ed/Assets/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager comp;
+    [SerializeField]
+    private List<Level> levels = new List<Level>();
+
+    public static GameManager _Components
+    {
+        get
+        {
+            return comp;
+        }
+    }
+
+    private void Awake()
+    {
+        Debug.Log("manager started");
+        if (comp == null)
+        {
+            comp = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (comp != this)
+                Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+
+    }
+}
---
+++ Virtual Ed/Assets/Level.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Level
+{
+    [SerializeField]
+    [Tooltip("time to finish level")]
+    private float timeLimit;
+
+    [SerializeField]
+    [Tooltip("time taken to complete level")]
+    private float completionTime;
+
+    [SerializeField]
+    [Tooltip("Number of protiens, nanoparticels, etc")]
+    private int score;
+
+    [SerializeField]
+    [Tooltip("Player support free 'points'")]
+    private int bonus;
+
+
+
+
+}
