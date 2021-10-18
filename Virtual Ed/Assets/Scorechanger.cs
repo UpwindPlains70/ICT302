1029ab440822c9b044489b899b41b7c57843e9c1
Manbeasto <Limo2012@hotmail.com>
2021-10-18 11:25:58 +0800

    removed the destroy on collision to stop the ball from despawning

--- Virtual Ed/Assets/Scorechanger.cs
+++ Virtual Ed/Assets/Scorechanger.cs
@@ -10,7 +10,6 @@ public class Scorechanger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        lvlManager = GameObject.FindGameObjectsWithTag("Score");
     }
 
     // Update is called once per frame
